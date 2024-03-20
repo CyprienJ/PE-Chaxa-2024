88af7d3c5e12e32ad7ff30cf478f7b5273b6838e
CyprienJ <jorantcyprien@protonmail.com>
2024-03-20 14:19:24 +0100

    Fix minimum size of injection fault widget in main window

--- mainWindow.py
+++ mainWindow.py
@@ -324,7 +324,7 @@ class Ui_MainWindow(object):
 
         # Widget for injection fault
         self.injection_Fault_widget = QtWidgets.QWidget(self.centralwidget)
-        self.injection_Fault_widget.setMinimumSize(QtCore.QSize(size.width() * 0.5, 700))
+        self.injection_Fault_widget.setMinimumSize(QtCore.QSize(size.width(), size.height()))
         self.injection_Fault_widget.setObjectName("injection_Fault_widget")
 
 
